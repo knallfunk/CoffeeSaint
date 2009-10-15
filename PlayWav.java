389fed71cf072342b7612d562bb8ec86be5147a5
folkert <folkert@425ee2a7-4875-4123-be73-356ab870d09e>
2009-10-15 08:36:54 +0000

    Initial revision
    git-svn-id: http://localhost/svn/cs/trunk@12 425ee2a7-4875-4123-be73-356ab870d09e

---
+++ PlayWav.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import javax.sound.sampled.*;
+ 
+public class PlayWav
+{
+	public PlayWav(String fileName) throws Exception
+	{
+		File soundFile = new File(fileName);
+		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+		AudioFormat format = audioInputStream.getFormat();
+		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+		SourceDataLine auline = (SourceDataLine) AudioSystem.getLine(info);
+
+		auline.open(format);
+		auline.start();
+
+		int nBytesRead = 0;
+		byte[] abData = new byte[128 * 1024];
+ 
+		while (nBytesRead != -1)
+		{
+			nBytesRead = audioInputStream.read(abData, 0, abData.length);
+			if (nBytesRead >= 0)
+				auline.write(abData, 0, nBytesRead);
+		}
+
+		auline.drain();
+		auline.close();
+	}
+}
