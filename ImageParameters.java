b8f774b82e189ac0ee33df7869743b6edb99bfda
folkert <folkert@425ee2a7-4875-4123-be73-356ab870d09e>
2009-11-02 13:38:26 +0000

    Initial revision
    git-svn-id: http://localhost/svn/cs/trunk@36 425ee2a7-4875-4123-be73-356ab870d09e

---
+++ ImageParameters.java
@@ -0,0 +1,36 @@
+import java.awt.Image;
+
+class ImageParameters
+{
+	Image img;
+	String fileName;
+	int width, height;
+
+	ImageParameters(Image img, String fileName, int width, int height)
+	{
+		this.img = img;
+		this.fileName = fileName;
+		this.width = width;
+		this.height = height;
+	}
+
+	Image getImage()
+	{
+		return img;
+	}
+
+	String getFileName()
+	{
+		return fileName;
+	}
+
+	int getWidth()
+	{
+		return width;
+	}
+
+	int getHeight()
+	{
+		return height;
+	}
+}
