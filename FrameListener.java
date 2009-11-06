804a8ed76b4e837374c53476a9d68070324b0aeb
folkert <folkert@425ee2a7-4875-4123-be73-356ab870d09e>
2009-11-06 21:10:49 +0000

    use Config object
    git-svn-id: http://localhost/svn/cs/trunk@39 425ee2a7-4875-4123-be73-356ab870d09e

--- FrameListener.java
+++ FrameListener.java
@@ -1,16 +1,24 @@
+/* Released under GPL2, (C) 2009 by folkert@vanheusden.com */
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 class FrameListener extends WindowAdapter
 {
+	Config config;
+
+	FrameListener(Config config)
+	{
+		this.config = config;
+	}
+
 	public void windowClosing(WindowEvent event)
 	{
 		try
 		{
-			if (CoffeeSaint.predictor != null)
+			if (CoffeeSaint.getPredictor() != null)
 			{
-				System.out.println("Storing brain to file " + CoffeeSaint.predictorBrainFileName);
-				CoffeeSaint.predictor.dumpBrainToFile(CoffeeSaint.predictorBrainFileName);
+				System.out.println("Storing brain to file " + config.getBrainFileName());
+				CoffeeSaint.predictor.dumpBrainToFile(config.getBrainFileName());
 			}
 		}
 		catch(Exception exception)
