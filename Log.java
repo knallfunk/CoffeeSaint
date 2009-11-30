e009a125f570e40241638f3b246fd8335daa3450
folkert <folkert@425ee2a7-4875-4123-be73-356ab870d09e>
2009-11-30 14:21:22 +0000

    Initial revision
    git-svn-id: http://localhost/svn/cs/trunk@123 425ee2a7-4875-4123-be73-356ab870d09e

---
+++ Log.java
@@ -0,0 +1,54 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.Semaphore;
+
+public class Log
+{
+	List<String> log = new ArrayList<String>();
+	int maxN = 100;
+	private Semaphore semaphore = new Semaphore(1);
+
+	private void lock()
+	{
+		semaphore.acquireUninterruptibly();
+	}
+
+	private void unlock()
+	{
+		semaphore.release();
+	}
+
+	public Log(int maxN)
+	{
+		this.maxN = maxN;
+	}
+
+	public String formatDate(Calendar when)
+	{
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("E yyyy.MM.dd  hh:mm:ss a zzz");
+
+		return dateFormatter.format(when.getTime());
+	}
+
+	public void add(String what)
+	{
+		System.out.println(what);
+
+		lock();
+		log.add(formatDate(Calendar.getInstance()) + "] " + what);
+
+		while (log.size() > maxN)
+			log.remove(0);
+		unlock();
+	}
+
+	public List<String> get()
+	{
+		List<String> copy = new ArrayList<String>();
+		lock();
+		for(String current : log)
+			copy.add(current);
+		unlock();
+		return copy;
+	}
+}
