46c15dda39f51e41a35c9acf51ae7fe460f7f3b4
folkert <folkert@425ee2a7-4875-4123-be73-356ab870d09e>
2009-12-18 14:12:45 +0000

    Initial revision
    git-svn-id: http://localhost/svn/cs/trunk@179 425ee2a7-4875-4123-be73-356ab870d09e

---
+++ BordersParameters.java
@@ -0,0 +1,32 @@
+public class BordersParameters
+{
+	protected int nProblems, nColumns, windowWidth, rowHeight;
+
+	public BordersParameters(int nProblems, int nColumns, int windowWidth, int rowHeight)
+	{
+		this.nProblems = nProblems;
+		this.nColumns = nColumns;
+		this.windowWidth = windowWidth;
+		this.rowHeight = rowHeight;
+	}
+
+	public int getNProblems()
+	{
+		return nProblems;
+	}
+
+	public int getNColumns()
+	{
+		return nColumns;
+	}
+
+	public int getWindowWidth()
+	{
+		return windowWidth;
+	}
+
+	public int getRowHeight()
+	{
+		return rowHeight;
+	}
+}
