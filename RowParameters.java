cf7d1cde20904378aa4893aa163302846400400e
folkert <folkert@425ee2a7-4875-4123-be73-356ab870d09e>
2009-12-18 13:04:02 +0000

    Initial revision
    git-svn-id: http://localhost/svn/cs/trunk@178 425ee2a7-4875-4123-be73-356ab870d09e

---
+++ RowParameters.java
@@ -0,0 +1,44 @@
+import java.awt.Font;
+
+public class RowParameters
+{
+	private int textWidth;
+	private boolean shrunkMore;
+	private double newAsc;
+	private Font f;
+	private double heightDiff;
+
+	public RowParameters(int textWidth, boolean shrunkMore, double newAsc, Font f, double heightDiff)
+	{
+		this.textWidth = textWidth;
+		this.shrunkMore = shrunkMore;
+		this.newAsc = newAsc;
+		this.f = f;
+		this.heightDiff = heightDiff;
+	}
+
+	public int getTextWidth()
+	{
+		return textWidth;
+	}
+
+	public boolean getShrunkMore()
+	{
+		return shrunkMore;
+	}
+
+	public double getAsc()
+	{
+		return newAsc;
+	}
+
+	public Font getAdjustedFont()
+	{
+		return f;
+	}
+
+	public double getHeightDiff()
+	{
+		return heightDiff;
+	}
+}
---
+++ ScrollableContent.java
@@ -0,0 +1,90 @@
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+public class ScrollableContent
+{
+	protected BufferedImage imageData;
+	protected int curPos;
+	protected int x, y;
+	protected int ww;
+	//
+	double scrollTs;
+
+	public ScrollableContent(int screenX, int screenY, int windowWidth)
+	{
+		curPos = 0;
+		scrollTs = System.currentTimeMillis() / 1000.0;
+		x = screenX;
+		y = screenY;
+		ww = windowWidth;
+	}
+
+	public ScrollableContent(BufferedImage newImage, int screenX, int screenY, int windowWidth)
+	{
+		curPos = 0;
+		scrollTs = System.currentTimeMillis() / 1000.0;
+		imageData = newImage;
+		x = screenX;
+		y = screenY;
+		ww = windowWidth;
+	}
+
+	public void setImage(BufferedImage newImage)
+	{
+		imageData = newImage;
+		adjustCurPos();
+	}
+
+	public boolean hasImage()
+	{
+		return imageData != null;
+	}
+
+	public int getCurPos()
+	{
+		return curPos;
+	}
+
+	protected void adjustCurPos()
+	{
+		while(curPos >= imageData.getWidth())
+			curPos -= imageData.getWidth();
+	}
+
+	public void scrollView(Graphics2D g2d, int scrollSpeed)
+	{
+		int imgWidth = imageData.getWidth();
+		int pixelsNeeded = ww;
+		int pixelsAvail = imgWidth - curPos;
+		int drawX = x, sourceX = curPos;
+		while(pixelsNeeded > 0)
+		{
+			int plotN = Math.min(pixelsNeeded, pixelsAvail);
+
+			g2d.drawImage((Image)imageData, drawX, y, drawX + plotN, y + imageData.getHeight(), sourceX, 0, sourceX + plotN, imageData.getHeight(), Color.GRAY, null);
+
+			pixelsNeeded -= plotN;
+			pixelsAvail -= plotN;
+			drawX += plotN;
+			sourceX += plotN;
+
+			if (pixelsAvail <= 0)
+			{
+				pixelsAvail = imgWidth;
+				sourceX = 0;
+			}
+		}
+
+		double scrollTsNow = (double)System.currentTimeMillis() / 1000.0;
+		// double scrollMultiplier = (scrollTsNow - scrollTs) / (5.0 / 1000.0);
+		double scrollMultiplier = 1.0;
+
+		curPos += (int)(((double)scrollSpeed / (1000.0 / 5.0)) * scrollMultiplier);
+
+		adjustCurPos();
+
+		scrollTs = scrollTsNow;
+	}
+}
