835940e360bd7122931d8a71eeb1c4639d7c9257
folkert <folkert@425ee2a7-4875-4123-be73-356ab870d09e>
2010-01-22 08:50:50 +0000

    Initial revision
    git-svn-id: http://localhost/svn/cs/trunk@223 425ee2a7-4875-4123-be73-356ab870d09e

---
+++ _FakeX509TrustManager.java
@@ -0,0 +1,25 @@
+import java.security.cert.X509Certificate;
+import java.security.cert.CertificateException;
+
+public class _FakeX509TrustManager implements javax.net.ssl.X509TrustManager {
+	private static final X509Certificate[] _AcceptedIssuers =
+		new X509Certificate[] {};
+
+	public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+		}
+
+	public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+		}
+
+	public boolean isClientTrusted(X509Certificate[] chain) {
+		return(true);
+	}
+
+	public boolean isServerTrusted(X509Certificate[] chain) {
+		return(true);
+	}
+
+	public X509Certificate[] getAcceptedIssuers() {
+		return(_AcceptedIssuers);
+	}
+}
